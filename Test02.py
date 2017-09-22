4d4947413c903f8e4317f9e2f6d5d32de0645bd6
Naoki Kohara <sdvr.nao@gmail.com>
2017-09-22 18:42:37 +0900

    不要ファイルの削除

--- Test02.py
+++
@@ -1,6 +0,0 @@
-y,m,d = map(str, input().split())
-
-while( int(y) < 2000 or int(y) > 2016) or ( int(m) < 1 or int(m) > 12 ) or ( int(d) < 1 or int(d) > 31):
-    y,m,d = map(str, input().split())
-
-print(y + '/'+m + '/' + d)
