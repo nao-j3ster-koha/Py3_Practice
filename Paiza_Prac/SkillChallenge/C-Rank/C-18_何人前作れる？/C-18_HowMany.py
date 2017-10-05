8bb1c34a2d2f2e2cff712caf2153d8dee662ddd0
Naoki Kohara <sdvr.nao@gmail.com>
2017-10-05 21:11:02 +0900

    文字列メソッドの調査

--- Paiza_Prac/SkillChallenge/C-Rank/C-18_何人前作れる？/C-18_HowMany.py
+++ Paiza_Prac/SkillChallenge/C-Rank/C-18_何人前作れる？/C-18_HowMany.py
@@ -1,44 +1,48 @@
 n = int(input())
-while ( n < 1 or n > 100):
-    n = int(input())
+#while ( n < 1 or n > 100):
+#    n = int(input())
 
 rcpList =[]
 
 for i in range(n):
     a1,b1 = map(str, input().split())
-    while( len(a1) < 1 or len(a1) > 10) or ( int(b1) < 1 or int(b1) > 100):
-        a1,b1 = map(str, input().split())
-
+#    while( len(a1) < 1 or len(a1) > 10) or ( int(b1) < 1 or int(b1) > 100):
+#        a1,b1 = map(str, input().split())
     rcpList.append([a1,b1])
 
 m = int(input())
-while ( m < 0 or m > 100):
-    m = int(input())
+#while ( m < 0 or m > 100):
+#    m = int(input())
 
 
 matDict = {}
-for j in range(m):
-    c1,d1 =map(str, input().split())
-    while( len(c1) < 1 or len(c1) > 10) or ( int(d1) < 1 or int(d1) > 10000):
+if ( m > 0):
+    for j in range(m):
         c1,d1 =map(str, input().split())
+#    while( len(c1) < 1 or len(c1) > 10) or ( int(d1) < 1 or int(d1) > 10000):
+#        c1,d1 =map(str, input().split())
+        matDict[c1] = d1
 
-    matDict[c1] = d1
-
-rsltList = []
-for k in range(n):
-    if ( rcpList[i][0] in matDict):
-        t = int(int(matDict[str(rcpList[i][0])]) / int(rcpList[i][1]))
+    rsltList = []
+    for k in range(n):
+        if ( rcpList[k][0] in matDict):
+            if(int(matDict[str(rcpList[k][0])]) > 0):
+                t = int(int(matDict[str(rcpList[k][0])]) / int(rcpList[k][1]))
+        else:
+            t = 0
         rsltList.append(t)
 
+    for p in range(len(rsltList)-1):
+        q = n - (p + 1)
+        for q in range(q,0,-1):
+            if(rsltList[q-1] > rsltList[q]):
+                w = rsltList[q-1]
+                rsltList[q-1] = rsltList[q]
+                rsltList[q] = w
+    print(rsltList[0])
+else:
+    rslt = 0
+    print(rslt)
 
-for p in range(len(rsltList)):
-    q = p + 1
-    for q in range(q,len(rsltList)-1,1):
-        if(rsltList[q-1] > rsltList[q]):
-            w = rsltList[q-1]
-            rsltList[q-1] = rsltList[q]
-            rsltList[q] = w
-
-print(rsltList[0])
 
 
